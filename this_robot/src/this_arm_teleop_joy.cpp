54ba1d6ec1f5cbf67f4768581b23612c088e4ca2
Aaron Parker <redbaron148@gmail.com>
2011-07-09 14:54:24 -0500

    created new node to publish goals, modified teleop to work w/o crustcrawler arm, created teleop launch file for crustcrawler arm

--- this_robot/src/this_arm_teleop_joy.cpp
+++ this_robot/src/this_arm_teleop_joy.cpp
@@ -29,6 +29,10 @@ TeleopThis::TeleopThis(): linear_(1), angular_(2)
     nh_.param("scale_linear", l_scale_, l_scale_);
 
     vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+    set_servo_position_client = nh_.serviceClient<crustcrawler_arm::SetServoPosition>("/set_servo_position");
+    get_servo_position_client = nh_.serviceClient<crustcrawler_arm::GetServoPosition>("/get_servo_position");
+    set_position_client = nh_.serviceClient<crustcrawler_arm::SetPosition>("/set_position");
+    set_all_servo_positions_client = nh_.serviceClient<crustcrawler_arm::SetAllServoPositions>("/set_all_servo_positions");
 
     joy_sub_ = nh_.subscribe<joy::Joy>("joy", 10, &TeleopThis::joyCallback, this);
 }
@@ -38,7 +42,23 @@ void TeleopThis::joyCallback(const joy::Joy::ConstPtr& joy)
     geometry_msgs::Twist vel;
     vel.linear.x = (a_scale_*joy->axes[angular_]);
     vel.angular.z = l_scale_*joy->axes[linear_];
-
+    if(joy->buttons[0])
+    {
+        if(gsp(CLAW) == OPEN)
+        {
+            ssp(CLAW,CLOSE);
+        }
+        else ssp(CLAW,OPEN);
+        std::cout << "doing stuff!" <<  std::endl;
+    }
+    if(joy->buttons[1])
+    {
+        sap(0,0,gsp(CLAW),0,0,0);
+    }
+    else if(joy->buttons[2])
+    {
+        set_position(0,12.5,4,-60);
+    }
     vel_pub_.publish(vel);
 }
 
